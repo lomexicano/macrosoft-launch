4aac3f8efa0cdcd6490d4eed1989dd7d39494a5b
Federal <gresendesa@gmail.com>
2020-07-15 01:55:49 -0300

    launcher done

--- src/main/java/net/minecraft/launcher/Macrosoft/Bootstrapper.java
+++ src/main/java/net/minecraft/launcher/Macrosoft/Bootstrapper.java
@@ -1,14 +1,27 @@
 package net.minecraft.launcher.Macrosoft;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,6 +32,7 @@ import com.google.gson.JsonArray;
 
 import net.minecraft.launcher.LauncherConstants;
 import net.minecraft.launcher.Macrosoft.ui.LaunchButton;
+import net.minecraft.launcher.ui.popups.login.LogInPopup;
 
 public class Bootstrapper {
 
@@ -28,41 +42,119 @@ public class Bootstrapper {
 	ArrayList<JButton> contexts = new ArrayList<JButton>();
 	
 	public Bootstrapper() {
-		frame = new JFrame("Macrosoft");
-		frame.setSize(200,200);
-		frame.setLocationRelativeTo(null);
+		
+		System.out.println("Loading Macrosoft Bootstrapper...");
+		
+		int frameHeight = 150;
+		String websiteLink = "https://webmacrosoft.herokuapp.com/";
+		String discordLink = "https://discord.gg/t7WcjJ4";
+		
+		frame = new JFrame("Macrosoft Launcher v" + LauncherConstants.MACROSOFT_VERSION);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		try {
 			JSONObject info = Connector.get("http://127.0.0.1:8000/launcher/info?format=json");
 			int version = (int)info.get("version");
-			if (0 <= LauncherConstants.MACROSOFT_VERSION) {
+			frameHeight = (int)info.get("menuHeight");
+			if (version < LauncherConstants.MACROSOFT_VERSION) {
 				JOptionPane.showMessageDialog(frame, "Your laucher is out-dated. Upgrade to version " + version, "Outdated", JOptionPane.WARNING_MESSAGE);
 			}
 			JSONArray servers = (JSONArray)info.get("servers");
-			System.out.println(version);
-			System.out.println(servers);
-			System.out.println(info);
 			for (Object object : servers) {
-				contexts.add(new JButton((String)object));
+				JButton button = new JButton((String)((JSONObject)object).get("name"));
+				String iconURL = (String)((JSONObject)object).get("icon");
+				try {
+					InputStream stream = new URL(iconURL).openStream();
+			        if (stream != null) {
+			            BufferedImage image = ImageIO.read(stream);
+			            Image resized = new ImageIcon(image).getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
+			            button.setIcon(new ImageIcon(resized));
+			        }
+				} catch (IOException e) {
+					System.out.println("Unable to load icon from " + iconURL);
+				}
+				
+				contexts.add(button);
 			}
 		} catch(IOException e) {
-			System.out.println(e.getMessage());
+			JOptionPane.showMessageDialog(frame, "Could not stablish the connection with Macrosoft Server", "Error connection", JOptionPane.ERROR_MESSAGE);
 		} catch (JSONException e) {
-			System.out.println(e.getMessage());
+			JOptionPane.showMessageDialog(frame, "Could not stablish the connection with Macrosoft Server", "Error connection", JOptionPane.ERROR_MESSAGE);
 		}
-	
-		//contexts.add(new JButton("Legacy"));
-		//contexts.add(new JButton("Chronos"));
-		//contexts.add(new JButton("Viciante"));
-		//contexts.add(new JButton("Outro"));
 		
+		frame.setSize(new Dimension(300,frameHeight));
+	
 		panel = new JPanel();
+
+		JButton defaultButton = new JButton("Other");
+		
+		try {
+			InputStream stream = JButton.class.getResourceAsStream("/minecraft_logo.png");
+	        if (stream != null) {
+	            BufferedImage image = ImageIO.read(stream);
+	            JLabel label = new JLabel(new ImageIcon(image));
+                panel.add(label);
+	        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			InputStream stream = JButton.class.getResourceAsStream("/mc.png");
+	        if (stream != null) {
+	            BufferedImage image = ImageIO.read(stream);
+	            Image resized = new ImageIcon(image).getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
+	            defaultButton.setIcon(new ImageIcon(resized));
+	        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+					
+		contexts.add(defaultButton);
 		
 		for (JButton jButton : contexts) {
 			panel.add(jButton);
 		}
 		
+		JButton link = new JButton("Website");
+		link.setBorderPainted(false);
+		
+		link.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    	try {
+		    		java.awt.Desktop.getDesktop().browse(java.net.URI.create(websiteLink));
+		    	} catch (IOException ex) {
+		    		ex.printStackTrace();
+		    	}
+	        }
+		});
+		panel.add(link);
+		
+		JButton linkSocial = new JButton("Discord");
+		linkSocial.setBorderPainted(false);
+		
+		linkSocial.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    	try {
+		    		java.awt.Desktop.getDesktop().browse(java.net.URI.create(discordLink));
+		    	} catch (IOException ex) {
+		    		ex.printStackTrace();
+		    	}
+	        }
+		});
+		panel.add(linkSocial);
+		
+		try {
+			InputStream stream = JButton.class.getResourceAsStream("/favicon.png");
+	        if (stream != null) {
+	            BufferedImage image = ImageIO.read(stream);
+	            frame.setIconImage(image);
+	        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		frame.setLocationRelativeTo(null);
 		frame.add(panel);
 		
 	}
--- src/main/java/net/minecraft/launcher/Macrosoft/Connector.java
+++ src/main/java/net/minecraft/launcher/Macrosoft/Connector.java
@@ -27,7 +27,6 @@ public class Connector {
     try {
       BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
       String jsonText = readAll(rd);
-      System.out.println(jsonText);
       JSONObject json = new JSONObject(jsonText);
       return json;
     } finally {
