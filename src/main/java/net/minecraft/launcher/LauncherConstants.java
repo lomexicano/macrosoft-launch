f9fc83fc5a9fdd1c85968bce2a1fe7f800d32b4d
Guilherme Sá <gresendesa@gmail.com>
2026-04-18 02:26:18 -0300

    Versão 9 OK

--- src/main/java/net/minecraft/launcher/LauncherConstants.java
+++ src/main/java/net/minecraft/launcher/LauncherConstants.java
@@ -16,7 +16,7 @@ import org.apache.commons.io.IOUtils;
 public class LauncherConstants {
     public static final int VERSION_FORMAT = 21;
     public static final String VERSION_STRING = "8";
-    public static final int MACROSOFT_VERSION = 7;
+    public static final int MACROSOFT_VERSION = 9;
     /** Domínios oficiais Macrosoft. O aviso de atualização só é exibido quando a API responde de um destes (ou subdomínios). */
     public static final String[] MACROSOFT_OFFICIAL_DOMAINS = {
         "macrosoft.website",
